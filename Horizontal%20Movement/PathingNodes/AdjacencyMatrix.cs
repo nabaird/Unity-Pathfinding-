4c2bc9679b8752198e6883d4a747afce4cc9c1aa
nabaird <nabaird@gmail.com>
2016-10-06 15:39:06 -0700

    Add files via upload

---
+++ Horizontal%20Movement/PathingNodes/AdjacencyMatrix.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AdjacencyMatrix : MonoBehaviour {
+
+    public class vertex
+    {
+        public GameObject node;
+        public float weight;
+
+        public vertex(GameObject n, float w)
+        {
+            node = n;
+            weight = w; 
+        }
+    }
+
+    public List<vertex> aMatrix;
+    public GameObject[] allNodes;
+    public List<GameObject> test;
+    public int index; 
+
+    public void GenerateMatrix()
+    {
+        allNodes = GameObject.Find("Nodes").GetComponent<AllNodes>().NodeList;
+        aMatrix = new List<vertex>();
+
+        foreach (GameObject node in allNodes)
+        {
+            float distance = Vector3.Distance(node.transform.position, transform.position);//the weight is the distance in unity between nodes
+            if (node != this)
+            {
+                RaycastHit hit;//We use raycasting to see which nodes within view of other nodes
+                if (Physics.Raycast(transform.position, Vector3.Normalize(node.transform.position - transform.position), out hit))
+                {
+                    if (hit.collider == node.GetComponent<Collider>())
+                    {
+                        aMatrix.Add(new vertex(node, distance));
+                    }
+                }
+            }
+        }
+
+        test = new List<GameObject>();
+        foreach (vertex v in aMatrix)
+        {
+            test.Add(v.node);
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Horizontal%20Movement/PathingNodes/AllNodes.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AllNodes : MonoBehaviour {
+
+    public GameObject[] NodeList;
+    public int size;
+    void Awake()
+    {
+       
+        NodeList = GameObject.FindGameObjectsWithTag("Node");
+       
+        for (int i=0; i<size; i++)
+        {
+            NodeList[i].GetComponent<AdjacencyMatrix>().index = i;
+            NodeList[i].GetComponent<AdjacencyMatrix>().GenerateMatrix();
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
